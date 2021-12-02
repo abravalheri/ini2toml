4ef16dc24d6c7869b5e671e2eb37272f6b4b12d3
Anderson Bravalheri <andersonbravalheri@gmail.com>
2021-12-02 23:48:00 +0000

    Use POPO for profile

--- src/ini2toml/profile.py
+++ src/ini2toml/profile.py
@@ -1,36 +1,55 @@
-from dataclasses import dataclass, field
-from dataclasses import replace as _replace
-from typing import List, Optional, TypeVar
+import inspect
+from typing import Optional, Sequence, TypeVar
 
 from .types import IntermediateProcessor, ProfileAugmentationFn, TextProcessor
 
 P = TypeVar("P", bound="Profile")
 
 
-@dataclass
+def replace(self: P, **changes) -> P:
+    """Works similarly to :func:`dataclasses.replace`"""
+    sig = inspect.signature(self.__class__)
+    kwargs = {x: getattr(self, x) for x in sig.parameters}
+    kwargs.update(changes)
+    return self.__class__(**kwargs)
+
+
 class Profile:
     """Profile object that follows the public API defined in
     :class:`ini2toml.types.Profile`.
     """
 
-    name: str
-    help_text: str = ""
-    pre_processors: List[TextProcessor] = field(default_factory=list)
-    intermediate_processors: List[IntermediateProcessor] = field(default_factory=list)
-    post_processors: List[TextProcessor] = field(default_factory=list)
-    ini_parser_opts: Optional[dict] = None
+    def __init__(
+        self,
+        name: str,
+        help_text: str = "",
+        pre_processors: Sequence[TextProcessor] = (),
+        intermediate_processors: Sequence[IntermediateProcessor] = (),
+        post_processors: Sequence[TextProcessor] = (),
+        ini_parser_opts: Optional[dict] = None,
+    ):
+        self.name = name
+        self.help_text = help_text
+        self.pre_processors = list(pre_processors)
+        self.intermediate_processors = list(intermediate_processors)
+        self.post_processors = list(post_processors)
+        self.ini_parser_opts = ini_parser_opts
 
-    def replace(self: P, **changes) -> P:
-        """See :func:`dataclasses.replace`"""
-        return _replace(self, **changes)
+    replace = replace
 
 
-@dataclass
 class ProfileAugmentation:
-    fn: ProfileAugmentationFn
-    active_by_default: bool = False
-    name: str = ""
-    help_text: str = ""
+    def __init__(
+        self,
+        fn: ProfileAugmentationFn,
+        active_by_default: bool = False,
+        name: str = "",
+        help_text: str = "",
+    ):
+        self.fn = fn
+        self.active_by_default = active_by_default
+        self.name = name
+        self.help_text = help_text
 
     def is_active(self, explicitly_active: Optional[bool] = None) -> bool:
         """``explicitly_active`` is a tree-state variable: ``True`` if the user
