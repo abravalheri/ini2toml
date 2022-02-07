02874338ae91c8a9f9278649b39f8d554ba06562
Anderson Bravalheri <andersonbravalheri@gmail.com>
2022-02-07 14:25:29 +0000

    Automatic update with PyScaffold

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
     Setup file for ini2toml.
     Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 4.1rc1.post1.dev19+g4c2abfd.
+    This file was generated with PyScaffold 4.1.4.
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
