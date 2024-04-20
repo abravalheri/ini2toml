795579341a05902a8bb1a3c5d7a259927d2c0fb3
Anderson Bravalheri <andersonbravalheri@gmail.com>
2024-04-20 12:45:07 +0100

    Add expectations about pytest.addopts removing comments

--- tests/plugins/test_pytest.py
+++ tests/plugins/test_pytest.py
@@ -1,48 +1,88 @@
 from textwrap import dedent
 
+import pytest
 import tomli
 
 from ini2toml.drivers import full_toml, lite_toml
-from ini2toml.plugins import pytest
-from ini2toml.translator import Translator
+from ini2toml.plugins.pytest import activate
+from ini2toml.translator import FullTranslator
+
+EXAMPLES = {
+    "simple": {
+        "example": """\
+            [pytest]
+            minversion = 6.0
+            addopts = -ra -q --cov ini2toml
+            testpaths = tests
+            python_files = test_*.py check_*.py example_*.py
+            required_plugins = pytest-django>=3.0.0,<4.0.0 pytest-html pytest-xdist>=1.0.0
+            norecursedirs =
+                dist
+                build
+                .tox
+            filterwarnings=
+                error
+                ignore:Please use `dok_matrix` from the `scipy\\.sparse` namespace, the `scipy\\.sparse\\.dok` namespace is deprecated.:DeprecationWarning
+            """,  # noqa
+        "expected": """\
+            [pytest]
+            [pytest.ini_options]
+            minversion = "6.0"
+            addopts = "-ra -q --cov ini2toml"
+            testpaths = ["tests"]
+            python_files = ["test_*.py", "check_*.py", "example_*.py"]
+            required_plugins = ["pytest-django>=3.0.0,<4.0.0", "pytest-html", "pytest-xdist>=1.0.0"]
+            norecursedirs = [
+                "dist", 
+                "build", 
+                ".tox", 
+            ]
+            filterwarnings = [
+                "error",
+                'ignore:Please use `dok_matrix` from the `scipy\\.sparse` namespace, the `scipy\\.sparse\\.dok` namespace is deprecated.:DeprecationWarning',
+            ]
+            """,  # noqa
+    },
+    "multiline-addopts-empty-first": {
+        "example": """\
+            [pytest]
+            addopts =
+               -ra -q
+               # comment
+               --cov ini2toml
+            """,
+        "expected": '''\
+            [pytest.ini_options]
+            addopts = """
+            -ra -q
+            --cov ini2toml
+            """
+            ''',
+    },
+    "multiline-addopts-no-empty": {
+        "example": """\
+            [pytest]
+            addopts = -ra -q
+               # comment
+               --cov ini2toml
+            """,
+        "expected": '''\
+            [pytest.ini_options]
+            addopts = """-ra -q
+            --cov ini2toml
+            """
+            ''',
+    },
+}
 
 
-def test_pytest():
-    example = """\
-    [pytest]
-    minversion = 6.0
-    addopts = -ra -q --cov ini2toml
-    testpaths = tests
-    python_files = test_*.py check_*.py example_*.py
-    required_plugins = pytest-django>=3.0.0,<4.0.0 pytest-html pytest-xdist>=1.0.0
-    norecursedirs =
-        dist
-        build
-        .tox
-    filterwarnings=
-        error
-        ignore:Please use `dok_matrix` from the `scipy\\.sparse` namespace, the `scipy\\.sparse\\.dok` namespace is deprecated.:DeprecationWarning
-    """  # noqa
-    expected = """\
-    [pytest]
-    [pytest.ini_options]
-    minversion = "6.0"
-    addopts = "-ra -q --cov ini2toml"
-    testpaths = ["tests"]
-    python_files = ["test_*.py", "check_*.py", "example_*.py"]
-    required_plugins = ["pytest-django>=3.0.0,<4.0.0", "pytest-html", "pytest-xdist>=1.0.0"]
-    norecursedirs = [
-        "dist", 
-        "build", 
-        ".tox", 
-    ]
-    filterwarnings = [
-        "error",
-        'ignore:Please use `dok_matrix` from the `scipy\\.sparse` namespace, the `scipy\\.sparse\\.dok` namespace is deprecated.:DeprecationWarning',
-    ]
-    """  # noqa
+@pytest.mark.parametrize("example_name", EXAMPLES.keys())
+def test_pytest(example_name):
+    example = EXAMPLES[example_name]["example"]
+    expected = EXAMPLES[example_name]["expected"]
+
     for convert in (lite_toml.convert, full_toml.convert):
-        translator = Translator(plugins=[pytest.activate], toml_dumps_fn=convert)
+        translator = FullTranslator(plugins=[activate], toml_dumps_fn=convert)
         out = translator.translate(dedent(example), "pytest.ini").strip()
         expected = dedent(expected).strip()
         print("expected=\n" + expected + "\n***")
